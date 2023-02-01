2784900af22b626ab683b5f882455dbb12b503a4
AakashSudhakar <aakash.sudhakar@gmail.com>
2023-01-31 23:48:35 -0500

    [ADD] Boilerplate LÖVE scripting

---
+++ main.lua
@@ -0,0 +1,19 @@
+/* 
+Main Project File for BYTEPATH Cloning Challenge. 
+
+The objective of this project is to successfully clone
+a functioning version of the indie game BYTEPATH using
+Lua and its relevant library, LÖVE. 
+*/
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
