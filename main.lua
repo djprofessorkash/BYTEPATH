dd75d7db7b7bfd8454ab97dc30bde393dd61c2a6
AakashSudhakar <aakash.sudhakar@gmail.com>
2023-02-02 11:42:37 -0500

    [STYLE] Clarified starter scripts with comments

--- main.lua
+++ main.lua
@@ -6,9 +6,11 @@ a functioning version of the indie game BYTEPATH using
 Lua and its relevant library, LÖVE.
 ]]--
 
+-- GLOBAL DEBUGGING SCRIPTS --
 if pcall(require, "lldebugger") then require("lldebugger").start() end
 if pcall(require, "mobdebug") then require("mobdebug").start() end
 
+-- HELLO WORLD FUNCTIONALITY --
 function love.draw()
     love.graphics.print("Hello World!", 100, 100)
 end
